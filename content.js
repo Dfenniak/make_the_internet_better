81285691c9b31b2b9c06a29328f5272f17f4ee0a
DFenniak <donald@getjobber.com>
2016-02-19 12:16:54 -0700

    initial commit

--- content.js
+++ content.js
@@ -1,15 +1,14 @@
 replaceInAllCases()
 function replaceInAllCases(){
-	replaceImages() //replace images when a page is first loaded.
-	//Below are for dealing with ajax and such. There is probably a better solution out there, but this worked fine for my intents.
+	$(document).ready(replaceImages())
 	$( window ).scroll(function() { replaceImages()	});
 	$( "div" ).hover(function() { replaceImages()	});
 }
 
 function replaceImages(){
-	var daveUrl = chrome.extension.getURL("dave.jpg")
+	var winnersUrl = chrome.extension.getURL("winners.jpg")
 	//replace images with the image of Dave, mark as done with new class
-	$('img:not(.daved)').attr("src", daveUrl).addClass("daved");
+	$('img:not(.winners)').attr("src", winnersUrl).addClass("winners");
 	//replace classes .img and .Image, mostly added this because Instagram doesn't use actual img objects
-	$('.Image:not(.daved), .img:not(.daved)').css("background-image", "url(" + daveUrl + ")").attr("src", daveUrl).addClass("daved");
+	$('.Image:not(.winners), .img:not(.winners)').css("background-image", "url(" + winnersUrl + ")").attr("src", winnersUrl).addClass("winners");
 }
